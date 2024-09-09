2a3c61e029e06c997fdd317dbbb3a34512665578
Ahmadd <azdaghmash@gmail.com>
2024-09-09 12:05:13 +0300

    fix the code by ading boolean function

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 import static java.lang.System.in;
@@ -6,22 +7,29 @@ import static java.lang.System.in;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner myproject = new Scanner(System.in);
-
-
+        Scanner myproject = new Scanner(in);
         System.out.println("whats your name?");
         String userName = myproject.nextLine();
         System.out.println("How old Are you");
         int age = myproject.nextInt();
-        if (age >= 18) {
-            System.out.println("Are you working?");
-            System.out.println("how much is your salary?");
-            double salary = myproject.nextDouble();
-        }
+        if (age >= 18 && age < 60) {
+            System.out.println("Are you working? (Yes / No)");
+            String answer = myproject.next();
+            if (answer.equals("Yes")) {
+                System.out.println("how much your salary?");
+                double Salary = myproject.nextDouble();
+            } else if (answer.equals("No")) {
+                System.out.println("User isn't working");
+            } else {
+                System.out.println("Something went wrong");
+            }
+        } else if (age >= 60) {
+            System.out.println("retired");
 
-             else
-                System.out.println("not working underage");
+        } else {
+            System.out.println("not working underage");
         }
     }
+}
 
 
